1df0f0d1934eb7fa150801b855a7468c8e07a65b
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-11-30 16:20:08 -0500

    [MICROBA-744] Added the edx.org theme to support future edx theme efforts.
    The changes included will allow us to change the theme of the Learner
    Record and keep the edx specific brand changes in this repo.

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 
 
-function generateConfig(theme) {
+function generateCertificateConfig(theme) {
     let config = {
         cache: true,
 
-        context: path.resolve(`./edx_credentials_themes/static/${theme}/`),
+        context: path.resolve(`./edx_credentials_themes/static/${theme}/certificates/`),
 
         entry: {},
 
@@ -64,10 +64,77 @@ function generateConfig(theme) {
 
     config.entry[`${theme}.certificate.style-ltr`] = './sass/certificate-ltr.scss';
     config.entry[`${theme}.certificate.style-rtl`] = './sass/certificate-rtl.scss';
-
     return config
 }
 
+function generateBaseConfig(theme) {
+    let config = {
+        cache: true,
 
-let targets = ['mitpe'].map(generateConfig);
+        context: path.resolve(`./edx_credentials_themes/static/${theme}/base/`),
+
+        entry: {},
+
+        output: {
+            path: path.resolve(`./edx_credentials_themes/static/${theme}/`),
+            filename: '[name]-[hash].js'
+        },
+
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].css'
+            })
+        ],
+
+        module: {
+            rules: [
+                {
+                    test: /\.s?css$/,
+                    use: [
+                      'style-loader',
+                      MiniCssExtractPlugin.loader,
+                      'css-loader',
+                      {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: [
+                                    path.join(__dirname, 'node_modules'),
+                                ],
+                            },
+                        },
+                      },
+                    ]
+                },
+                {
+                    test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[name].[ext]',
+                            publicPath: '../'
+                        }
+                    }]
+
+                }
+            ]
+        },
+        resolve: {
+            modules: ['./node_modules'],
+            extensions: ['.css', '.js', '.scss']
+        }
+    };
+
+    config.entry[`${theme}.base.style-ltr`] = './sass/main-ltr.scss';
+    config.entry[`${theme}.base.style-rtl`] = './sass/main-rtl.scss';
+    return config
+}
+
+// Change back to ['edx.org'].map(generateCertificateConfig) once we add in
+// the need to style Program certificates
+let targets = [];
+let baseTargets = ['edx.org'].map(generateBaseConfig);
+for (configIndex in baseTargets){
+  targets.push(baseTargets[configIndex]);
+}
 module.exports = targets;
