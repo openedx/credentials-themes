f646b4e7628bc476ff7d70f0bb220c943dece78d
Thomas Tracy <contact@tjtracy.com>
2020-12-08 15:00:49 -0500

    Rebrand and webpack rework
    First wave of rebrand work. I've edited the webpack to combine both the
    certificate and base styles bundling, along with removal of errant files
    that were being generated (we don't need js bundles, theres no js in
    this repo).

--- webpack.config.js
+++ webpack.config.js
@@ -3,138 +3,69 @@ const glob = require('glob');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 
-
-function generateCertificateConfig(theme) {
-    let config = {
-        cache: true,
-
-        context: path.resolve(`./edx_credentials_themes/static/${theme}/certificates/`),
-
-        entry: {},
-
-        output: {
-            path: path.resolve(`./edx_credentials_themes/static/${theme}/`),
-            filename: '[name]-[hash].js'
-        },
-
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].css'
-            })
-        ],
-
-        module: {
-            rules: [
-                {
-                    test: /\.s?css$/,
-                    use: [
-                      'style-loader',
-                      MiniCssExtractPlugin.loader,
-                      'css-loader',
-                      {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                includePaths: [
-                                    path.join(__dirname, 'node_modules'),
-                                ],
-                            },
-                        },
-                      },
-                    ]
-                },
-                {
-                    test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                    use: [{
-                        loader: 'file-loader',
-                        options: {
-                            name: 'fonts/[name].[ext]',
-                            publicPath: '../'
-                        }
-                    }]
-
-                }
-            ]
-        },
-        resolve: {
-            modules: ['./node_modules'],
-            extensions: ['.css', '.js', '.scss']
-        }
-    };
-
-    config.entry[`${theme}.certificate.style-ltr`] = './sass/certificate-ltr.scss';
-    config.entry[`${theme}.certificate.style-rtl`] = './sass/certificate-rtl.scss';
-    return config
-}
-
 function generateBaseConfig(theme) {
-    let config = {
-        cache: true,
+  let config = {
+    cache: true,
 
-        context: path.resolve(`./edx_credentials_themes/static/${theme}/base/`),
-
-        entry: {},
-
-        output: {
-            path: path.resolve(`./edx_credentials_themes/static/${theme}/`),
-            filename: '[name]-[hash].js'
-        },
-
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: 'css/[name].css'
-            })
-        ],
-
-        module: {
-            rules: [
-                {
-                    test: /\.s?css$/,
-                    use: [
-                      'style-loader',
-                      MiniCssExtractPlugin.loader,
-                      'css-loader',
-                      {
-                        loader: 'sass-loader',
-                        options: {
-                            sassOptions: {
-                                includePaths: [
-                                    path.join(__dirname, 'node_modules'),
-                                ],
-                            },
-                        },
-                      },
-                    ]
+    context: path.resolve(`./edx_credentials_themes/static/${theme}`),
+    entry: {
+      [`${theme}.base.style-ltr`]: './base/sass/main-ltr.scss',
+      [`${theme}.base.style-rtl`]: './base/sass/main-rtl.scss',
+      [`${theme}.certificate.style-ltr`]: './certificates/sass/certificate-ltr.scss',
+      [`${theme}.certificate.style-rtl`]: './certificates/sass/certificate-rtl.scss',
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].css'
+      })
+    ],
+    module: { 
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  includePaths: [
+                    path.join(__dirname, 'node_modules'),
+                  ],
                 },
-                {
-                    test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                    use: [{
-                        loader: 'file-loader',
-                        options: {
-                            name: 'fonts/[name].[ext]',
-                            publicPath: '../'
-                        }
-                    }]
-
-                }
-            ]
+              },
+            },
+          ]
         },
-        resolve: {
-            modules: ['./node_modules'],
-            extensions: ['.css', '.js', '.scss']
-        }
-    };
+        {
+          test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+          use: [{
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name].[ext]',
+              publicPath: '../'
+            }
+          }]
 
-    config.entry[`${theme}.base.style-ltr`] = './sass/main-ltr.scss';
-    config.entry[`${theme}.base.style-rtl`] = './sass/main-rtl.scss';
-    return config
+        }
+      ]
+    },
+    resolve: {
+      modules: ['./node_modules'],
+      extensions: ['.css', '.js', '.scss']
+    }
+  };
+
+  return config
 }
 
 // Change back to ['edx.org'].map(generateCertificateConfig) once we add in
 // the need to style Program certificates
 let targets = [];
 let baseTargets = ['edx.org'].map(generateBaseConfig);
-for (configIndex in baseTargets){
+for (configIndex in baseTargets) {
   targets.push(baseTargets[configIndex]);
 }
 module.exports = targets;
