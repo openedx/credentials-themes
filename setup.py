a4601895c35a08825d5587897f9f51aaf3266dc5
M. Zulqarnain <muhammad.zulqarnain@arbisoft.com>
2021-09-13 19:00:12 +0500

    feat: Django 3.2 Support (#306)

--- setup.py
+++ setup.py
@@ -78,5 +78,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Framework :: Django',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
     ],
 )
